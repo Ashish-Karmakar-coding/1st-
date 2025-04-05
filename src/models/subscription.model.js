c9f7e7f9298e97e89aad1a86558a10a801a0cae2
Ashish Karmakar <ashish.karmakar782@gmail.com>
2025-04-05 20:53:51 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from 'mongoose';
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribin
+        ref: 'User',
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // channel to which the user is subscribing
+        ref: 'User',
+    },
+
+},{timestamps: true}); // to add timestamps to the schema
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
